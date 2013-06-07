cbf6a0c382cf6d029dc714a048678bca07fd436f
Tomás Rafael Moreno Poggio <sirlordt@gmail.com>
2013-06-07 11:56:34 -0430

    Separate LoginModule to another independent project

--- AbstractModules/src/AbstractModules/CUIAbstractModule.java
+++ AbstractModules/src/AbstractModules/CUIAbstractModule.java
@@ -7,12 +7,24 @@ import org.zkoss.zk.ui.Execution;
 
 public abstract class CUIAbstractModule implements IAbstractModule {
 
+	protected String strIconSet = "";
+	
+	public void setIconSet( String strIconSet ) {
+		
+		this.strIconSet = strIconSet;
+		
+	}
+	
 	public enum DesktopPosition { Top, Left, Bottom, Right, Center, Overlap };
 	
     public abstract Component getUIComponents( Execution CurrentExecution, Component Parent, @SuppressWarnings("rawtypes") HashMap Args );
 
-    public abstract String getIconPath( String strSize );
+    public abstract String getMainIconPath( String strIconSize );
     
     public abstract DesktopPosition getDesktopPosition();
+    
+    public abstract String getTranslateLabel( String strLabel );
+    
+    public abstract String getImagePath( String strIconName );
     
 }
